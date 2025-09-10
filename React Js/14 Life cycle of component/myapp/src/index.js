11131c925f4488a74e985fbb5f1181cef92407cd
gauravKothari <kotharigaurav6@gmail.com>
2025-09-10 08:31:19 +0530

    react

--- React Js/14 Life cycle of component/myapp/src/index.js
+++ React Js/14 Life cycle of component/myapp/src/index.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {favoritecolor: "red"};
+    this.state = {show: true};
   }
-  shouldComponentUpdate() {
-    // return false;
-    return true;
-  }
-  changeColor = () => {
-    this.setState({favoritecolor: "blue"});
+  delHeader = () => {
+    this.setState({show: false});
   }
   render() {
+    let myheader;
+    if (this.state.show) {
+      myheader = <Child />;
+    };
     return (
       <div>
-      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-      <button type="button" onClick={this.changeColor}>Change color</button>
+      {myheader}
+      <button type="button" onClick={this.delHeader}>Delete Header</button>
       </div>
     );
   }}
+class Child extends React.Component {
+  componentWillUnmount() {
+    alert("The component named Header is about to be unmounted.");
+  }
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }}
 
-  createRoot(document.getElementById('root')).render(<App/>);
+createRoot(document.getElementById('root')).render(<App/>);
